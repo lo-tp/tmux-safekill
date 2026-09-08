7ddb463b4490bbbf810414ca97270d0396566dd6
lo-tp <regesteraccount@hotmail.com>
2026-09-08 10:36:10 +0800

    Add pi process support (send 'Escape Escape 0 C "/quit" Enter')

--- scripts/safekill.sh
+++ scripts/safekill.sh
@@ -18,6 +18,8 @@ function safe_end_procs {
       cmd='Enter "~."'
     elif [[ "$pane_proc" == "psql" ]]; then
       cmd='Enter "\q"'
+    elif [[ "$pane_proc" == "pi" ]]; then
+      cmd='Escape Escape 0 C "/quit" Enter'
     fi
     echo $cmd | xargs tmux send-keys -t "$pane_id"
   done
